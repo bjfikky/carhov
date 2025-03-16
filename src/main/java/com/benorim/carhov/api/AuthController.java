a8b5d2e6e67371c9d41dc30b2a3f29bb08e72eb2
Benjamin Orimoloye <bjfikky@yahoo.com>
2025-03-16 14:43:43 -0400

    Added AuthController

---
+++ src/main/java/com/benorim/carhov/api/AuthController.java
@@ -0,0 +1,101 @@
+package com.benorim.carhov.api;
+
+import com.benorim.carhov.dto.auth.JwtResponseDTO;
+import com.benorim.carhov.dto.auth.LoginRequestDTO;
+import com.benorim.carhov.dto.auth.MessageResponseDTO;
+import com.benorim.carhov.dto.auth.SignupRequestDTO;
+import com.benorim.carhov.dto.auth.TokenRefreshRequestDTO;
+import com.benorim.carhov.dto.auth.TokenRefreshResponseDTO;
+import com.benorim.carhov.entity.RefreshToken;
+import com.benorim.carhov.exception.TokenRefreshException;
+import com.benorim.carhov.repository.CarHovUserRepository;
+import com.benorim.carhov.security.jwt.JwtUtils;
+import com.benorim.carhov.security.services.RefreshTokenService;
+import com.benorim.carhov.security.services.UserDetailsImpl;
+import com.benorim.carhov.service.AuthService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/auth")
+public class AuthController {
+    final AuthenticationManager authenticationManager;
+
+    final CarHovUserRepository userRepository;
+
+    final JwtUtils jwtUtils;
+
+    final RefreshTokenService refreshTokenService;
+
+    private final AuthService authService;
+
+    @PostMapping("/signin")
+    public ResponseEntity<?> authenticateUser(@Valid @RequestBody LoginRequestDTO loginRequest) {
+        Authentication authentication = authenticationManager
+                .authenticate(new UsernamePasswordAuthenticationToken(loginRequest.getEmail(), loginRequest.getPassword()));
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+
+        UserDetailsImpl userDetails = (UserDetailsImpl) authentication.getPrincipal();
+
+        String jwt = jwtUtils.generateJwtToken(authentication);
+
+        List<String> roles = userDetails.getAuthorities().stream()
+                .map(item -> item.getAuthority())
+                .collect(Collectors.toList());
+
+        RefreshToken refreshToken = refreshTokenService.createRefreshToken(userDetails.getId());
+
+        return ResponseEntity.ok(new JwtResponseDTO(jwt, refreshToken.getToken(),
+                userDetails.getId(),
+                userDetails.getUsername(),
+                roles));
+    }
+
+    @PostMapping("/signup")
+    public ResponseEntity<?> registerUser(@Valid @RequestBody SignupRequestDTO signUpRequest) {
+        if (userRepository.existsByEmail(signUpRequest.getEmail())) {
+            return ResponseEntity.badRequest().body(new MessageResponseDTO("Error: Email is already in use!"));
+        }
+
+        authService.registerUser(signUpRequest);
+
+        return ResponseEntity.ok(new MessageResponseDTO("User registered successfully!"));
+    }
+
+    @PostMapping("/refreshtoken")
+    public ResponseEntity<?> refreshToken(@Valid @RequestBody TokenRefreshRequestDTO request) {
+        String requestRefreshToken = request.getRefreshToken();
+
+        return refreshTokenService.findByToken(requestRefreshToken)
+                .map(refreshTokenService::verifyExpiration)
+                .map(RefreshToken::getUser)
+                .map(user -> {
+                    String token = jwtUtils.generateTokenFromUsername(user.getEmail());
+                    return ResponseEntity.ok(new TokenRefreshResponseDTO(token, requestRefreshToken));
+                })
+                .orElseThrow(() -> new TokenRefreshException(requestRefreshToken,
+                        "Refresh token is not in database!"));
+    }
+
+    @PostMapping("/signout")
+    public ResponseEntity<?> logoutUser() {
+        UserDetailsImpl userDetails = (UserDetailsImpl) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        Long userId = userDetails.getId();
+        refreshTokenService.deleteByUserId(userId);
+        return ResponseEntity.ok(new MessageResponseDTO("Log out successful!"));
+    }
+}
