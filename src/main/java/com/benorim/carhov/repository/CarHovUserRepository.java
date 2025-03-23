3477d99bdf35e28c92c1f30f949042dbf51acb35
Benjamin Orimoloye <bjfikky@yahoo.com>
2025-03-23 15:25:43 -0400

    Added findByRole to CarHovUserRepository

--- src/main/java/com/benorim/carhov/repository/CarHovUserRepository.java
+++ src/main/java/com/benorim/carhov/repository/CarHovUserRepository.java
@@ -2,12 +2,18 @@ package com.benorim.carhov.repository;
 
 import com.benorim.carhov.entity.CarHovUser;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface CarHovUserRepository extends JpaRepository<CarHovUser, Long> {
     Optional<CarHovUser> findByEmail(String email);
     Boolean existsByEmail(String email);
+
+    @Query("SELECT c FROM CarHovUser c INNER JOIN c.roles r WHERE r.name = :roleName")
+    List<CarHovUser> findByRole(@Param("roleName") String roleName);
 }
