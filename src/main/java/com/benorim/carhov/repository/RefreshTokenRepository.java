beca697517d494e56594dcfbf4c73db974033599
Benjamin Orimoloye <bjfikky@yahoo.com>
2025-03-23 15:27:59 -0400

    Added deleteByExpiryDateBefore to RefreshTokenRepository

--- src/main/java/com/benorim/carhov/repository/RefreshTokenRepository.java
+++ src/main/java/com/benorim/carhov/repository/RefreshTokenRepository.java
@@ -1,17 +1,21 @@
 package com.benorim.carhov.repository;
 
-import com.benorim.carhov.entity.RefreshToken;
 import com.benorim.carhov.entity.CarHovUser;
+import com.benorim.carhov.entity.RefreshToken;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.time.Instant;
 import java.util.Optional;
 
 @Repository
 public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
     Optional<RefreshToken> findByToken(String token);
+
+    @Transactional
+    int deleteByExpiryDateBefore(Instant now);
     
-    @Modifying
+    @Transactional
     int deleteByUser(CarHovUser user);
 }
