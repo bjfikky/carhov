8ae8849af47c7587f4a53eaaa80caf7a951bd0ab
Benjamin Orimoloye <bjfikky@yahoo.com>
2025-03-28 00:00:06 -0400

    Cleaned up test annotations

--- src/test/java/com/benorim/carhov/PostgreSQLContainerTest.java
+++ src/test/java/com/benorim/carhov/PostgreSQLContainerTest.java
@@ -2,18 +2,14 @@ package com.benorim.carhov;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
 import org.testcontainers.containers.PostgreSQLContainer;
-import org.testcontainers.junit.jupiter.Testcontainers;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
-@Testcontainers
 @Import(TestcontainersConfiguration.class)
-@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 class PostgreSQLContainerTest {
 
      @Autowired
