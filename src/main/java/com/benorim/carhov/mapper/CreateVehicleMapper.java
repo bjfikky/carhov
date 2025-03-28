45ef302e7afe681f8ad217d82696a18d118f6f24
Benjamin Orimoloye <bjfikky@yahoo.com>
2025-03-28 00:02:06 -0400

    More vehicle details to mapper

--- src/main/java/com/benorim/carhov/mapper/CreateVehicleMapper.java
+++ src/main/java/com/benorim/carhov/mapper/CreateVehicleMapper.java
@@ -1,8 +1,6 @@
 package com.benorim.carhov.mapper;
 
-import com.benorim.carhov.dto.rideSchedule.RideScheduleDTO;
 import com.benorim.carhov.dto.vehicle.VehicleDTO;
-import com.benorim.carhov.entity.RideSchedule;
 import com.benorim.carhov.entity.Vehicle;
 
 public class CreateVehicleMapper {
@@ -15,8 +13,9 @@ public class CreateVehicleMapper {
         dto.setMake(vehicle.getMake());
         dto.setModel(vehicle.getModel());
         dto.setColor(vehicle.getColor());
+        dto.setYear(vehicle.getYear());
         dto.setLicensePlate(vehicle.getLicensePlate());
-        dto.setCapacity(vehicle.getCapacity());
+        dto.setPassengerCapacity(vehicle.getPassengerCapacity());
 
         return dto;
     }
